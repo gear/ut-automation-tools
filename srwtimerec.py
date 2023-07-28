9c192c331086ea52badc999245b78fb5c1b58d72
Hoang NT <hoangnt.titech@gmail.com>
2023-07-28 10:31:40 +0900

    clock

---
+++ srwtimerec.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from time import sleep
+from argsparse import ArgumentParser
+
+
+parser = ArgumentParser()
+parser.add_argument('username')
+parser.add_argument('password')
+parser.add_argument('mode')
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    web = webdriver.Chrome(options=options)
+
+    web.get("https://ut-ppsweb.adm.u-tokyo.ac.jp/cws/srwtimerec")
+    sleep(5)
+    assert web.title == 'WEB打刻', 'Cannot fetch website'
+
+    uname = web.find_element(By.NAME, 'user_id')
+    passw = web.find_element(By.NAME, 'password')
+    in_button = web.find_element(By.NAME, 'syussya')
+    out_button = web.find_element(By.NAME, 'taisya')
+
+    uname.send_keys(args.username)
+    passw.send_keys(args.password)
+
+    if args.mode == 'in':
+        in_button.click()
+    else:
+        out_button.click()
+

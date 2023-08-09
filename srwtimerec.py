f851d5c3e199b24f6d05d098a9efbaa2a62b0895
Hoang NT <hoangnt.titech@gmail.com>
2023-08-09 17:40:17 +0900

    add holidays

--- srwtimerec.py
+++ srwtimerec.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from time import sleep
+from datetime import datetime
 from argparse import ArgumentParser
 
 
@@ -10,27 +11,37 @@ parser.add_argument('username')
 parser.add_argument('password')
 parser.add_argument('mode')
 
+holidays = {
+    "23-08-11",
+    "23-08-14",
+    "23-08-15"
+}
+
 
 if __name__ == "__main__":
-    args = parser.parse_args()
-    options = Options()
-    options.add_argument("--headless=new")
-    options.add_experimental_option("excludeSwitches", ["enable-logging"])
-    web = webdriver.Chrome(options=options)
-
-    web.get("https://ut-ppsweb.adm.u-tokyo.ac.jp/cws/srwtimerec")
-    sleep(5)
-    assert web.title == 'WEB打刻', 'Cannot fetch website'
-
-    uname = web.find_element(By.NAME, 'user_id')
-    passw = web.find_element(By.NAME, 'password')
-    in_button = web.find_element(By.NAME, 'syussya')
-    out_button = web.find_element(By.NAME, 'taisya')
-
-    uname.send_keys(str(args.username))
-    passw.send_keys(str(args.password))
-
-    if args.mode == 'in':
-        web.execute_script("arguments[0].click();", in_button)
+    today = datetime.now().strftime("%y-%m-%d")
+    if today in holidays:
+        print("Skip logging in due to holiday")
     else:
-        web.execute_script("arguments[0].click();", out_button)
+        args = parser.parse_args()
+        options = Options()
+        options.add_argument("--headless=new")
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        web = webdriver.Chrome(options=options)
+
+        web.get("https://ut-ppsweb.adm.u-tokyo.ac.jp/cws/srwtimerec")
+        sleep(5)
+        assert web.title == 'WEB打刻', 'Cannot fetch website'
+
+        uname = web.find_element(By.NAME, 'user_id')
+        passw = web.find_element(By.NAME, 'password')
+        in_button = web.find_element(By.NAME, 'syussya')
+        out_button = web.find_element(By.NAME, 'taisya')
+
+        uname.send_keys(str(args.username))
+        passw.send_keys(str(args.password))
+
+        if args.mode == 'in':
+            web.execute_script("arguments[0].click();", in_button)
+        else:
+            web.execute_script("arguments[0].click();", out_button)
